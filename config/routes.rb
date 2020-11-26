9a6da014525a8a3b570f7169a52e831d5a5cb7e1
Kundan Raj <kundanr387@gmail.com>
2020-11-26 10:38:07 +0530

    Added friends and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'contact/index'
+  get 'friend/index'
   get 'test/index'
-  #get 'dashboard/index'
+  get 'dashboard/index'
   devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
-  get 'home/index'
-  #root 'home#index'
+  #get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
